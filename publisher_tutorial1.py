c238a0c9aa7fd4b24cbd01211992409e4c162e07
sahabi <sahabi@gmail.com>
2016-11-08 16:46:16 -0600

    edited

---
+++ publisher_tutorial1.py
@@ -0,0 +1,13 @@
+# create a node
+# publish your command to /cmd
+
+def get_input():
+	key_input = raw_input("What's your move? ")
+	return key_input
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
