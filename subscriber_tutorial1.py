cac9c00c667863138c2d712c9de8e5a6768618de
sahabi <sahabi@gmail.com>
2016-11-08 16:48:43 -0600

    edited

--- subscriber_tutorial1.py
+++ subscriber_tutorial1.py
@@ -11,21 +11,12 @@ def main():
 
     sim = Simulation("config.txt")
 
-    #sim.load_matrix_file("matrix.txt") --only include if a matrix file is used
-
-    # sim.load_slip_file("slip.txt") #turns slipping to 'on'
-
-    state = sim.get_state() #grab state
-
     done = False
 
     while not done:
 
         done, state = sim.move(["south"]) #main call
 
-        # print(sim.get_history(2)["agents"]) #get the history from 2 time steps back
-        
-    #sim.generate_agent_matrix("agent_matrix.txt")
 
 
 main()
